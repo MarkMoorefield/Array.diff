7c98fc9e90c2fa4896e9c192a784567de25e5cfb
MarkMoorefield <155267237+MarkMoorefield@users.noreply.github.com>
2024-01-16 21:29:11 +0000

    Added the reasoning behind the solution.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// The first thing I did was create the function that takes in a and b as the two arrays.
+// The next thing I did was grab a and attach the filter method to it. 
+// In the filter I asked it to check every element (elem) and check that it DOESN'T include b.
+// It then returns the elements that aren't the value of b. 
 function arrayDiff(a, b) {
     return a.filter((elem) => !b.includes(elem))
    }
